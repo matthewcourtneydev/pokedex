0b8d18577512109267973d9dd6a25595f8a6536d
Matthew Courtney <mdcdev23@gmail.com>
2023-09-10 18:01:21 -0400

    mdc_pokedex added in pages and routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Home from "./pages/home";
+import Login from './pages/login';
+import Register from './pages/register';
+import User from './pages/user';
+import Pokedex from './pages/pokedex';
 
 function App() {
   return (
     <div className="App">
-      Hello World
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/user" element={<User />} />
+        <Route path="/pokedex" element={<Pokedex />} />
+      </Routes>
     </div>
   );
 }
