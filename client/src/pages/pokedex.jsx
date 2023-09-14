59a46f9ab958cfbb8cb74adf7c5ab78ae342b3ca
Matthew Courtney <mdcdev23@gmail.com>
2023-09-13 21:02:35 -0400

    mdc_pokedex calling the pokemon list and storing the data. now i will create a card to display each pokemon

--- client/src/pages/pokedex.jsx
+++ client/src/pages/pokedex.jsx
@@ -1,11 +1,26 @@
-import React from 'react';
+import React, { useEffect } from "react";
 
 const Pokedex = () => {
-    return (
-        <div>
-            Pokedex
-        </div>
-    );
-}
+  let pokemonList = {};
+
+  async function getPokemon() {
+    const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=151");
+    return response.json();
+  }
+
+  useEffect(() => {
+    getPokemon().then((data) => {
+      pokemonList = data
+    });
+  }, []);
+
+
+
+  return (
+    <div id="pokemon-list">
+        
+    </div>
+  )
+};
 
 export default Pokedex;
