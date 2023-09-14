c0709776a02fad019671b1d74367a3e7816596aa
Matthew Courtney <mdcdev23@gmail.com>
2023-09-14 00:13:17 -0400

    mdc_pokedex added context to the user page

--- client/src/pages/user.jsx
+++ client/src/pages/user.jsx
@@ -1,9 +1,14 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UserContext } from '../contexts/userContext';
 
 const User = () => {
+    const userData = useContext(UserContext);
+    const user = userData.user;
     return (
         <div>
-            User
+            <h1>{user.username}</h1>
+            <h2>{user.email}</h2>
+            <p>{user.starter}</p>
         </div>
     );
 }
