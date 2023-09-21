6ce752b8eedb195416e1f5dd9cf32e1ef070b3f6
Matthew Courtney <mdcdev23@gmail.com>
2023-09-20 22:06:15 -0400

    mdc_pokedex styled the home page

--- client/src/pages/home.jsx
+++ client/src/pages/home.jsx
@@ -1,13 +1,28 @@
-import React, { useContext, useEffect } from 'react';
-import { UserContext } from '../contexts/userContext';
+import React, { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../contexts/userContext";
+import img from "../imgs/pokedex.png";
 
 const Home = () => {
-    const user = useContext(UserContext);
-    return (
-        <div>
-            <h1>HomePage</h1>
-        </div>
-    );
-}
+  const user = useContext(UserContext);
+  const navigate = useNavigate();
+
+  function rediretToRegister() {
+    navigate('/login')
+  }
+  return (
+    <div className="homepage">
+      <div className="homepage-content">
+        <img src={img} alt="" />
+        <img
+          src="https://i.pinimg.com/originals/35/ba/a2/35baa23613f80db94564d2fe5c1ecce6.png"
+          alt=""
+        />
+        <p>Start your journey today</p>
+        <button className="register" onClick={rediretToRegister} >Sign Up Now</button>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
